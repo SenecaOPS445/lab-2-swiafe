5c903cb0b2a6579281aef3bc0c9d2a684af84031
swiafe <swiafe@myseneca.ca>
2024-05-22 15:23:53 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: Samuel WIafe
+
+# Author ID: swiafe
+
+# Date Created: 2024-05-22
+ 
+import sys
+ 
+timer = int(sys.argv[1])
+ 
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
