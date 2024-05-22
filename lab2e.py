a7629cf7d0dcbff1c33916521d3e5d5294ce4a2b
swiafe <swiafe@myseneca.ca>
2024-05-22 15:01:46 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Author: Samuel Wiafe
+# Author ID: swiafe
+# Date Created: 2024-05-22
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
