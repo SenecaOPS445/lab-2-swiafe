ee9a33e45e96d7dc406203c4af0cab395ca82055
swiafe <swiafe@myseneca.ca>
2024-05-22 13:05:04 -0400

    Added lab2b

---
+++ lab2b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+name = 'Jon'
+age = 20
+
+name = 'Jen'
+age = 25
+
+name = input('Name: ')
+age = input('Age: ')
+
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
